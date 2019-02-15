9244cb9a87a92420a5d6d9e83e35475e5ab0ae20
Paolo Marzolo <pollo.marzo@gmail.com>
2019-02-15 22:52:36 +0100

    Added filler, never tested

--- Translator.c
+++ Translator.c
@@ -5,7 +5,11 @@
 #include "stack.h"
 #include "command.h"
 
+
 /*
+PROBLEMS: Never tested filler
+Expected solution: prova a vedere se va e fa quel che dovrebbe. mi pare dovrebbe andare
+
 -> Cose da fare:
   ~> implementazione stack
   ~> MAIN.c:
@@ -14,16 +18,71 @@
     => remove comments and remove spaces
   ~> STACK.c:
     => push
--> Problemi
 */
+
+typedef struct command{
+  char instr[200];
+  char type[200];
+  int number;
+} command;
+
+
+
+command* filler(char* nospace, command* current_parse){   //written like this so that "  push '\t'   constant '\t''\t' 9   " creates no problem
+  int i = 0, j = 0;
+  char numero[20]={'\0'};                                 //={'\0'} this should make lines 60-61 useless but not sure
+  int instr = 0, type = 0, num = 0;
+
+  current_parse->instr[0] = '\0';                           //initialize correctly to avoid problems
+  current_parse->type[0] = '\0';
+  current_parse->number = -1;                               //I hope I'm allowed to do this
+
+  while((nospace[i]==' ' || nospace[i] == '\t') && nospace[i]!='\0') i++; //move forward until end of space and tab
+  while((nospace[i]!=' ' && nospace[i] != '\t') && nospace[i]!='\0') {    //copy until tab is found
+    current_parse->instr[j] = nospace[i];                                 //placing it inside of struct as instr
+    j++;
+    i++;
+    instr = 1;                                                            //acts as boolean value in case in case I forgot something
+  }
+  j = 0;
+
+  while((nospace[i]==' ' || nospace[i] == '\t') && nospace[i]!='\0') i++; //move forward until end of space and tab
+  while((nospace[i]!=' ' && nospace[i] != '\t') && nospace[i]!='\0') {
+    current_parse->type[j] = nospace[i];                                  //placing it inside of struct as type
+    j++:
+    i++;
+    type = 1;                                                             //acts as boolean value in case in case I forgot something
+  }
+  j = 0;
+
+  while((nospace[i]==' ' || nospace[i] == '\t') && nospace[i]!='\0') i++; //move forward until end of space and tab
+  while((nospace[i]!=' ' && nospace[i] != '\t') && nospace[i]!='\0') {
+    numero[j] = nospace[i];                                               //saving it for conversion
+    j++;
+    i++;
+    num = 1;                                                              //needed to know it atoi is necessary
+  }
+  if (num == 1){
+    j = 0;
+    while (numero[j] >= '0' && numero[j] <= '9') j++;                     // I don't trust atoi so this is just to make sure
+    numero[j] = '\0';                                                     //juuuuust to make sure.... ._.
+    current_parse->number=atoi(number)
+  }
+}
+
 
 command *parser(char *c){
   char nospace[200];
 
-  remove_space(nospace, c);                               //rimuove gli spazi
+//remove_space(nospace, c);                               //rimuove gli spazi
   remove_comment(nospace);                                //rimuove i commenti
   command *current_parse = malloc(sizeof(command));       //istanzio uno spazio pari a 'command'
   command *trash = malloc(sizeof(command));               //istanzio una nuova variabile in caso sia un comando inutile
+  trash->instr[0]='\0';
+  trash->type[0]='\0';
+
+  filler(nospace, current_parse);
+  return current_parse;                                             //ritorno il puntatore
 }
 
 int main(int argc, char **argv){
