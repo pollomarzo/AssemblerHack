061846126dbfbf22c40c58595feeee5835299ba0
mackness33 <davidantonhy962@gmail.com>
2019-02-15 17:11:44 +0100

    Iniziamo 2

---
+++ Translator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+-> Cose da fare:
+  ~> implementazione stack
+  ~> MAIN.c:
+    => fileIn and fileOut (PAOLO)
+    => ciclo fgets
+    => remove comments and remove spaces
+  ~> STACK.c:
+    => push
+*/
+
+int main(int argc, char **argv){
+
+  return 0;
+}
