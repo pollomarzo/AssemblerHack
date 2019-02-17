bfe9566791cc5601929c997574093767b58d8f54
Paolo Marzolo <pollo.marzo@gmail.com>
2019-02-17 12:05:33 +0100

    sistemato filler, testato

--- Translator.c
+++ Translator.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 #include "stack.h"
-#include "command.h"
+// #include "command.h"
 
 
 /*
@@ -26,32 +26,48 @@ typedef struct command{
   int number;
 } command;
 
+//REMOVE COMMENTS
+//rimuove i commenti
+void remove_comment(char str[200]) {
+  char c = '/';
+  int i=0;
+  while (i < 20) {
+    if (str[i] == c)
+      str[i] = '\0';
+    i = i + 1;
+  }
+}
 
-
-command* filler(char* nospace, command* current_parse){   //written like this so that "  push '\t'   constant '\t''\t' 9   " creates no problem
+void filler(char* nospace, command* current_parse){   //written like this so that "  push '\t'   constant '\t''\t' 9   " creates no problem
   int i = 0, j = 0;
   char numero[20]={'\0'};                                 //={'\0'} this should make lines 60-61 useless but not sure
   int instr = 0, type = 0, num = 0;
 
+
   current_parse->instr[0] = '\0';                           //initialize correctly to avoid problems
   current_parse->type[0] = '\0';
   current_parse->number = -1;                               //I hope I'm allowed to do this
-
-  while((nospace[i]==' ' || nospace[i] == '\t') && nospace[i]!='\0') i++; //move forward until end of space and tab
+  printf("%s", "ciaooo sono prima dei loop");
+  while((nospace[i]==' ' || nospace[i] == '\t') && nospace[i]!='\0'){
+   //     printf("%s", "ciaooo sono nel loop che cerca il primo buco");
+  i++;  //move forward until end of space and tab
+  }
   while((nospace[i]!=' ' && nospace[i] != '\t') && nospace[i]!='\0') {    //copy until tab is found
     current_parse->instr[j] = nospace[i];                                 //placing it inside of struct as instr
     j++;
     i++;
     instr = 1;                                                            //acts as boolean value in case in case I forgot something
+  //  printf("%s", "ciaooo sono nel loop instr");
   }
   j = 0;
 
   while((nospace[i]==' ' || nospace[i] == '\t') && nospace[i]!='\0') i++; //move forward until end of space and tab
   while((nospace[i]!=' ' && nospace[i] != '\t') && nospace[i]!='\0') {
     current_parse->type[j] = nospace[i];                                  //placing it inside of struct as type
-    j++:
+    j++;
     i++;
     type = 1;                                                             //acts as boolean value in case in case I forgot something
+ // printf("%s", "ciaooo sono nel loop type");
   }
   j = 0;
 
@@ -61,13 +77,15 @@ command* filler(char* nospace, command* current_parse){   //written like this so
     j++;
     i++;
     num = 1;                                                              //needed to know it atoi is necessary
+ // printf("%s", "ciaooo sono nel loop numero");
   }
   if (num == 1){
     j = 0;
     while (numero[j] >= '0' && numero[j] <= '9') j++;                     // I don't trust atoi so this is just to make sure
     numero[j] = '\0';                                                     //juuuuust to make sure.... ._.
-    current_parse->number=atoi(number)
+    current_parse->number=atoi(numero);
   }
+  printf("%d", current_parse->number);
 }
 
 
@@ -75,7 +93,8 @@ command *parser(char *c){
   char nospace[200];
 
 //remove_space(nospace, c);                               //rimuove gli spazi
-  remove_comment(nospace);                                //rimuove i commenti
+  remove_comment(c);                                //rimuove i commenti
+  strcpy(nospace,c);
   command *current_parse = malloc(sizeof(command));       //istanzio uno spazio pari a 'command'
   command *trash = malloc(sizeof(command));               //istanzio una nuova variabile in caso sia un comando inutile
   trash->instr[0]='\0';
@@ -97,6 +116,7 @@ int main(int argc, char **argv){
 
   while(fgets(instr, sizeof(instr), filein)!=NULL){         //while ci sono righe di grandezza massima 'instr' nel file 'filein'. Se ci sono assegnarle alla variabile (instr).
     current = parser(instr);
-
+    fprintf(fileout, "%d", current->number);
+  }
   return 0;
 }
